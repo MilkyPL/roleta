b46c754c89c92b857a8a403066550f319f59ed95
MilkyPL <kubex.jb@gmail.com>
2017-09-21 16:12:29 +0200

    setup stuff

--- roleta.ino
+++ roleta.ino
@@ -1,11 +1,13 @@
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-void setup() {
-  // put your setup code here, to run once:
+LiquidCrystal_I2C lcd(0x27,20,4);
 
+void setup() {
+  lcd.init();
+  lcd.init();
+  lcd.backlight();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
